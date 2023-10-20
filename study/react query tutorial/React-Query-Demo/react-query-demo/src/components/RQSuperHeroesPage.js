d1799028bf40ee0003f97e30eae73c9c7880a83e
zenghyun <pipo36@naver.com>
2023-10-20 23:19:33 +0900

    study 'How to different isLoading & isFetching'

--- study/react query tutorial/React-Query-Demo/react-query-demo/src/components/RQSuperHeroesPage.js
+++ study/react query tutorial/React-Query-Demo/react-query-demo/src/components/RQSuperHeroesPage.js
@@ -23,7 +23,7 @@ export const RQSuperHeroesPage = () => {
     // refetchOnWindowFocus: true, // 즉시 업데이트 기능
     // refetchInterval: false, // false가 기본값 2000이면 쿼리가 2초마다 다시 가져옴
     // refetchIntervalInBackground: false, // 브라우저에 focus되어 있지 않아도 refetch를 시켜주는 것을 의미한다.
-    // enabled: false,
+    enabled: false,
     // select(data) { // select 옵션을 사용하면 반환된 데이터의 일부를 변환하거나 선택할 수 있다.
     //   const superHeroNames = data.data.map((hero) => hero.name);
     //   return superHeroNames
@@ -32,11 +32,16 @@ export const RQSuperHeroesPage = () => {
   console.log({ isLoading, isFetching });
   console.log("fetchStatus:", fetchStatus, "status:", status);
 
-  // const handleClickRefetch = useCallback(() => {
-  //   refetch();
-  // }, [refetch]);
+  const handleClickRefetch = useCallback(() => {
+    refetch();
+  }, [refetch]);
 
-  if (isLoading) {
+ /**
+  * isLoading && isFetching 해야하는 이유 
+   https://despiteallthat.tistory.com/307 
+  * 
+  */
+  if (isLoading && isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -47,7 +52,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
-      {/* <button onClick={handleClickRefetch}>Fetch Heroes</button> */}
+      <button onClick={handleClickRefetch}>Fetch Heroes</button>
       <ul>
         {data?.data.map((hero) => {
           return <li key={hero.name}>{hero.name}</li>;
